0e61cb16d4e229c4f01d23c9c91d5ca106224933
Ernestoyoofi <94425296+ernestoyoofi@users.noreply.github.com>
2025-06-28 11:58:42 +0700

    Fix: react frontend

--- frontend/bank-jateng/src/main.jsx
+++ frontend/bank-jateng/src/main.jsx
@@ -1,34 +1,66 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import Home from './pages/Home.jsx';
-import Register from './pages/Register.jsx';
-import Dashboard from './pages/Dashboard.jsx';
-import Karyawan from './pages/Karyawan.jsx';
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router"
+import Home from "./pages/Home.jsx"
+import Register from "./pages/Register.jsx"
+import Dashboard from "./pages/Dashboard.jsx"
+import Karyawan from "./pages/Karyawan.jsx"
+import Header from "./components/Header.jsx"
 
-
+// let router = createBrowserRouter([
+//   {
+//     path: "/",
+//     Component: Home,
+//   },
+//   {
+//     path: "/register",
+//     Component: Register,
+//   },
+//   {
+//     path: "/dashboard",
+//     Component: Dashboard,
+//   },
+//   {
+//     path: "/karyawan",
+//     Component: Karyawan,
+//   }
+// ]);
 let router = createBrowserRouter([
   {
-    path: '/',
-    Component: Home,
-  },
-  {
-    path: '/register',
-    Component: Register,
-  },
-  {
-    path: '/dashboard',
-    Component: Dashboard,
-  },
-  {
-    path: '/karyawan',
-    Component: Karyawan,
+    path: "/",
+    Component: Header,
+    children: [
+      {
+        index: true,
+        
+        Component: Home
+      },
+      {
+        path: "register",
+        Component: Register,
+      },
+      {
+        path: "dashboard",
+        Component: Dashboard,
+      },
+      {
+        path: "karyawan",
+        Component: Karyawan,
+      }
+    ]
   }
-]);
+])
 
-createRoot(document.getElementById('root')).render(
+function MainRoot() {
+  return <>
+    <RouterProvider
+      router={router}
+    />
+  </>
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-    {/* <App /> */}
+    <MainRoot />
   </StrictMode>,
 )
